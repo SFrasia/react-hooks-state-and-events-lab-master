77f94662cabaa3def7dec30f901801539a6558b5
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-03-06 14:57:55 +0300

    Changes

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ShoppingList from "./ShoppingList";
+import itemData from "../data/items";
+import react, { useState } from "react";
+
+function App() {
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const appClass = isOn ? "App dark" : "App light";
+
+  return (
+    <div className={appClass}>
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={handleClick}>
+          {isOn ? "Dark Mode" : "Light Mode"}
+        </button>
+      </header>
+      <ShoppingList items={itemData} />
+    </div>
+  );
+}
+
+export default App;
